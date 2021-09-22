b7ea3037422250ea21e35eee1080a935e7c1f354
subhananam <subhandragon@gmail.com>
2021-09-22 21:02:21 +0700

    Add test case

--- tests/unit/RadioButton.spec.js
+++ tests/unit/RadioButton.spec.js
@@ -16,3 +16,21 @@ describe("RadioButton.vue", () => {
     expect(wrapper.find(".sub-component-title").text()).toMatch(text);
   });
 });
+describe("RadioButton.vue", () => {
+  it("Find Radio Input Text", () => {
+    const wrapper = shallowMount(RadioButton);
+    expect(wrapper.find("#radio-parent-1").).toBeDefined();
+  });
+});
+describe("RadioButton.vue", () => {
+  it("Find Radio Date Picker", () => {
+    const wrapper = shallowMount(RadioButton);
+    expect(wrapper.find("#radio-parent-2")).toBeDefined();
+  });
+});
+describe("RadioButton.vue", () => {
+  it("Find Radio More Complex Form", () => {
+    const wrapper = shallowMount(RadioButton);
+    expect(wrapper.find("#radio-parent-3")).toBeDefined();
+  });
+});
